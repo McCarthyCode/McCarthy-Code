5dcf34c4a80cba7b3d748d2f022259c097d99611
Matt McCarthy <mattmc318@gmail.com>
2019-11-04 20:50:40 -0600

    Made title responsive

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(() => {
   $('a.external-link')
     .append(' <i class="fas fa-external-link-alt" title="External Link"></i>');
+  
+  function adjustTitle() {
+    let width = $(this).width();
+    let $title = $('header .h1');
+
+    if (width < 624) {
+      $title.css('-webkit-transform', `translate(-50%, -50%) scale(${width / 624})`);
+      $title.css('-ms-transform', `translate(-50%, -50%) scale(${width / 624})`);
+      $title.css('transform', `translate(-50%, -50%) scale(${width / 624})`);
+    } else {
+      $title.css('-webkit-transform', 'translate(-50%, -50%)');
+      $title.css('-ms-transform', 'translate(-50%, -50%)');
+      $title.css('transform', 'translate(-50%, -50%)');
+    }
+  }
+
+  adjustTitle();
+  $(window).on('resize orientationchange', adjustTitle);
 });
