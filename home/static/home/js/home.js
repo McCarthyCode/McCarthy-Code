ea63852045096c91256647b7ba500abc2e13bb2d
Matt McCarthy <mattmc318@gmail.com>
2020-03-27 18:01:28 -0500

    Updated footer to include start year; changed adjustments of title box and added adjustments to contact box

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -3,21 +3,69 @@ $(document).ready(() => {
     let width = $(this).width();
     let $title = $('header .h1');
 
-    if (width < 624) {
-      $title.css('-webkit-transform', `translate(-50%, -50%) scale(${width / 624})`);
-      $title.css('-ms-transform', `translate(-50%, -50%) scale(${width / 624})`);
-      $title.css('transform', `translate(-50%, -50%) scale(${width / 624})`);
+    const breakpoint = 624;
+    const scale = width / breakpoint;
+    if (width < breakpoint) {
+      $title.css({
+        '-webkit-transform':
+          `translate(-50%, -50%) scale(${scale})`,
+        '-ms-transform':
+          `translate(-50%, -50%) scale(${scale})`,
+        'transform':
+          `translate(-50%, -50%) scale(${scale})`,
+      });
     } else {
-      $title.css('-webkit-transform', 'translate(-50%, -50%)');
-      $title.css('-ms-transform', 'translate(-50%, -50%)');
-      $title.css('transform', 'translate(-50%, -50%)');
+      $title.css({
+        '-webkit-transform': `translate(-50%, -50%)`,
+        '-ms-transform': `translate(-50%, -50%)`,
+        'transform': `translate(-50%, -50%)`,
+      });
     }
+
+    const height = $title.outerHeight();
+    $('header').prop('min-height', `${height * scale + 32}px`);
+  }
+  function adjustContact() {
+    let width = $(this).width();
+    let $contact = $('section:nth-child(4) > div');
+
+    const breakpoint = 359;
+    const scale = width / breakpoint;
+
+    if (width < breakpoint) {
+      $contact.css({
+        '-webkit-transform':
+          `translate(-50%, -50%) scale(${scale})`,
+        '-ms-transform':
+          `translate(-50%, -50%) scale(${scale})`,
+        'transform':
+          `translate(-50%, -50%) scale(${scale})`,
+      });
+    } else {
+      $contact.css({
+        '-webkit-transform': `translate(-50%, -50%)`,
+        '-ms-transform': `translate(-50%, -50%)`,
+        'transform': `translate(-50%, -50%)`,
+      });
+    }
+
+    const winHeight = $(window).outerHeight();
+    const navbarHeight = $('#navbar').outerHeight();
+    const footerHeight = $('#footer').outerHeight();
+    const contactHeight = $contact.outerHeight();
+
+    $('section:nth-child(4)').css('min-height', `${Math.max(winHeight - navbarHeight - footerHeight, contactHeight * Math.min(1, scale) + 32)}px`);
   }
 
-  adjustTitle();
-  $(window).on('resize orientationchange', adjustTitle);
+  function adjust() {
+    adjustTitle();
+    adjustContact();
+  }
 
-  let navHeight = $('nav').first().outerHeight();
+  adjust();
+  $(window).on('resize orientationchange', adjust);
+
+  let navHeight = $('#navbar').outerHeight();
   $('header .chevron i').click(() => {
     $('html, body').animate({
       scrollTop: $('section:nth-child(3)').offset().top - navHeight,
