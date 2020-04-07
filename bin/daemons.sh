54a6693710c5304b0ab22c87e8f12c1ab8c4fc02
Matt McCarthy <mattmc318@gmail.com>
2020-04-06 19:25:46 -0500

    Minor configuration updates

--- bin/daemons.sh
+++ bin/daemons.sh
@@ -6,7 +6,7 @@ sass_output_home_compressed="home/static/home/css/home.min.css"
 
 declare -a foo=()
 declare -a daemons=(
-  "source env/bin/activate; python manage.py runserver 10.0.0.100:8000"
+  "source .env/bin/activate; python manage.py runserver 10.0.0.100:8000"
   "sass --watch $sass_input_home:$sass_output_home"
   "sass --watch --style=compressed $sass_input_home:$sass_output_home_compressed"
 )
