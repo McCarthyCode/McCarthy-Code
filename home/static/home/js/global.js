2e6c9434e7dd148b6f2dc29aeb1004160e316cba
Matt McCarthy <mattmc318@gmail.com>
2020-04-06 21:36:09 -0500

    Added attribution to footer; replaced under construction verbiage with navigation icons

--- home/static/home/js/global.js
+++ home/static/home/js/global.js
@@ -1,4 +1,4 @@
 $(document).ready(() => {
   $('a.external-link')
-    .append(' <i class="fas fa-external-link-alt" title="External Link"></i>');
+    .after(' <i class="fas fa-external-link-alt" title="External Link"></i>');
 });
