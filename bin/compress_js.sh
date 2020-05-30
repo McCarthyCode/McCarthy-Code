acf03ccb708e012988965679bfa4f0febd2c53f2
Matt McCarthy <matt@mccarthywebdesign.com>
2020-05-30 10:51:57 -0500

    Added links to navbar

--- bin/compress_js.sh
+++ bin/compress_js.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-closure_compiler="/opt/closure-compiler/closure-compiler-v20190929.jar"
+closure_compiler="bin/closure-compiler-v20200504.jar"
 
-js_input_global="home/static/home/js/global.js"
-js_output_global="home/static/home/js/global.min.js"
-
-js_input_index="home/static/home/js/index.js"
-js_output_index="home/static/home/js/index.min.js"
-
-js_input_portfolio="home/static/home/js/portfolio.js"
-js_output_portfolio="home/static/home/js/portfolio.min.js"
-
-declare -a commands=(
-  # "java -jar $closure_compiler --js $js_input_global --js_output_file $js_output_global"
-  # "java -jar $closure_compiler --js $js_input_index --js_output_file $js_output_index"
-  # "java -jar $closure_compiler --js $js_input_portfolio --js_output_file $js_output_portfolio"
+declare -a files=(
+  "home/static/home/js/global"
+  "home/static/home/js/index"
+  "home/static/home/js/portfolio"
 )
 
-for i in "${commands[@]}"; do
-  echo "$i"
-  $i
+for i in "${files[@]}" ; do
+  input="$i.js"
+  output="$i.min.js"
+
+  cmd="java -jar $closure_compiler --js $input --js_output_file $output"
+
+  if [ $input -nt $output ] ; then
+    echo "$cmd"
+    $cmd
+  fi
 done
--- home/static/home/js/global.js
+++ home/static/home/js/global.js
@@ -1,4 +1,17 @@
 $(document).ready(() => {
+  // external link icon
   $('a.external-link')
     .after('<i class="fas fa-external-link-alt" title="External Link"></i>');
+
+  // navbar menu
+  $('#navbarMenuButton').click(() => {
+    $('#navbarCollapse').slideToggle();
+  });
+  $(window).on('resize orientationchange', () => {
+    if ($(this).width() >= breakpointMd) {
+      $('#navbarCollapse').css('display', 'inline');
+    } else {
+      $('#navbarCollapse').hide();
+    }
+  });
 });
