a34bc4e700957b03c2fd281f32e50f44ef9383b8
Matt McCarthy <matt@mccarthywebdesign.com>
2020-06-08 20:38:12 -0500

    Upgrade Django to v3.0.7

--- home/static/home/js/portfolio.js
+++ home/static/home/js/portfolio.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
   const navHeight = $('#navbar').outerHeight();
 
+  // scroll buttons
   $('#portfolio > section:not(:last-of-type)').each(function () {
     let $next = $(this).next();
 
@@ -10,4 +11,23 @@ $(document).ready(() => {
       }, 1000);
     });
   });
+
+  // resize sections
+  function resizeSections() {
+    $('section').each(function () {
+      const $this = $(this);
+      const $container = $this.find('.container');
+
+      const mobileHeight = $this.find('.mobile').outerHeight();
+      const desktopHeight = $this.find('.desktop').outerHeight();
+
+      const maxHeight = Math.max(mobileHeight, desktopHeight);
+
+      $this.css('height', maxHeight + 32 + 64);
+      $container.css('height', maxHeight + 32);
+    });
+  };
+
+  resizeSections();
+  $(window).on('resize orientationchange', resizeSections);
 });
