2bd731c1e944dcb9c1edcf30ae30dbc21cf1fa9f
Matt McCarthy <matt@mccarthywebdesign.com>
2020-06-25 14:55:04 -0500

    Update portfolio.js to reflect previous changes

--- home/static/home/js/portfolio.js
+++ home/static/home/js/portfolio.js
@@ -5,29 +5,10 @@ $(document).ready(() => {
   $('#portfolio > section:not(:last-of-type)').each(function () {
     let $next = $(this).next();
 
-    $(this).find('.chevron i').click(function () {
+    $(this).find('.chevron').click(function () {
       $('html, body').animate({
         scrollTop: $next.offset().top - navHeight,
       }, 1000);
     });
   });
-
-  // resize sections
-  function resizeSections() {
-    $('section').each(function () {
-      const $this = $(this);
-      const $container = $this.find('.container');
-
-      const mobileHeight = $this.find('.mobile').outerHeight();
-      const desktopHeight = $this.find('.desktop').outerHeight();
-
-      const maxHeight = Math.max(mobileHeight, desktopHeight);
-
-      $this.css('height', maxHeight + 32 + 64);
-      $container.css('height', maxHeight + 32);
-    });
-  };
-
-  resizeSections();
-  $(window).on('resize orientationchange', resizeSections);
 });
