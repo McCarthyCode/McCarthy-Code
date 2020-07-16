54614b6d49c1b5563a51023f961b4627215e98d1
Matt McCarthy <matt@mccarthywebdesign.com>
2020-07-15 19:44:32 -0500

    Add Facebook Pixel code
    - Adjust spacing in HTML head and in settings.py

--- mwd/settings.py
+++ mwd/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Retrieve production stage environment variable
 class MissingEnvironmentVariable(Exception):
     pass
@@ -34,6 +35,7 @@ elif STAGE == 'production':
     DEBUG = False
 else:
     raise InvalidEnvironmentVariable('The value of environment variable STAGE is not valid.')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -57,6 +59,7 @@ elif STAGE == 'production':
         'mccarthywebdesign.com',
         'www.mccarthywebdesign.com',
     ]
+
 
 # Application definition
 
